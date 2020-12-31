7692f64570161e407c78074d69d9d694912b38ae
devdhacoder <adigunolamide200@gmail.com>
2020-12-31 23:54:59 +0100

    navigation, and mobile view responsiveness

---
+++ .history/index_20201230172833.js
@@ -0,0 +1,376 @@
+window.addEventListener("load", (e) => {
+    // alert("Page fully loaded")
+
+    const index = 0;
+    const buttons = document.querySelectorAll(".btn");
+    const navbar = document.querySelector(".nav-bar");
+    const ham = document.querySelector(".mobile-nav");
+    const menuBar = document.querySelector(".menu-bar");
+    const commentSlider = document.querySelector(".comment-slider-display");
+    const comments = [
+        {
+            index: 0,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Alex",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 1,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Daniel",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 2,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Totton",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 3,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Totton",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 4,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Totton",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 5,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Totton",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+        {
+            index: 6,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+            name: "Sampson Fodden",
+            job: "UI/UX Designer",
+            comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+        },
+    ]
+
+
+    const commentList = comments.map(comment => ( `
+    <div class="comment-card">
+        <div class="comment-card-header">
+            <div class="comment-header-display">
+                <div class="comment-profile-image-container">
+                    <div class="comment-image-container">
+                        <img class="comment-img" src=${comment.img} alt="">
+                    </div>
+                </div>
+                <div class="comment-profile-detail">
+                    <p>${comment.name}</p>
+                    <div class="comment-center">
+
+                    </div>
+                    <p>${comment.job}</p>
+                </div>
+            </div>
+
+            <div class="comment-number">
+                <p>${comment.index}</p>
+            </div>
+        </div>
+
+        
+        <div class="comment-details">
+            <p>${comment.comment}.</p>
+        </div>
+    </div>
+    
+    `
+    ))
+
+    commentSlider.innerHTML = commentList;
+
+
+
+
+    ham.addEventListener("click", () => {
+        if (navbar.classList.toggle("showNav")) {
+            menuBar.src = "./assets/images/cancel.png"
+        } else {
+            // ham.classList.toggle("showClose");
+            menuBar.src = "./assets/images/menu.png"
+        }
+        
+    })
+
+
+
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function (event)  {
+            if(e) {
+                // if (index === 0) {
+                //     index = comments.length
+                // }
+
+                // index--
+                console.log("Right")
+                commentSlider.scrollLeft -= 180;
+            
+            }
+            if(e.target.classList.contains("leftBtn")) {
+                // index++
+                // if (index === comments.length) {
+                //     index = 0;
+                // }
+
+                console.log("Left")
+
+                commentSlider.scrollLeft += 180;
+
+            
+                
+            }
+        })
+    })
+
+
+
+
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("load", (e) => {
+//     // alert("Page fully loaded")
+
+//     const index = 0;
+//     const button = document.querySelectorAll(".bnt");
+//     const commentSlider = document.querySelector(".comment-slider-display");
+//     const comments = [
+//         {
+//             index: 0,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 1,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 2,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 3,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 4,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 5,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//         {
+//             index: 6,
+//             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHPI4syIBtEAiO1wDxmwyKzorNnuvXArOBhQ&usqp=CAU",
+//             name: "Sampson Totton",
+//             job: "UI/UX Designer",
+//             comment: "We are happy to hear you had a positive experience at Noren! We value your input and encourage you to let us know more details about your experience with us."
+//         },
+//     ]
+
+
+//     commentSlider.innerHTML = `
+//     <div class="comment-card">
+//         <div class="comment-card-header">
+//             <div class="comment-header-display">
+//                 <div class="comment-profile-image-container">
+//                     <div class="comment-image-container">
+//                         <img class="comment-img" src=${comments[index].img} alt="">
+//                     </div>
+//                 </div>
+//                 <div class="comment-profile-detail">
+//                     <p>${comments[index].name}</p>
+//                     <div class="comment-center">
+
+//                     </div>
+//                     <p>${comments[index].job}</p>
+//                 </div>
+//             </div>
+
+//             <div class="comment-number">
+//                 <p>${comments[index].index}</p>
+//             </div>
+//         </div>
+
+        
+//         <div class="comment-details">
+//             <p>${comments[index].comment}.</p>
+//         </div>
+//     </div>
+    
+//     `
+
+
+
+
+
+
+//     button.forEach((button) => {
+//         button.addEventListener("click", (e) => {
+//             if(e.target.parentElement.classList.contains("right-btn")) {
+//                 if (index === 0) {
+//                     index = comments.length
+//                 }
+
+//                 index--
+//                 commentSlider.innerHTML = `
+//                 <div class="comment-card">
+//                     <div class="comment-card-header">
+//                         <div class="comment-header-display">
+//                             <div class="comment-profile-image-container">
+//                                 <div class="comment-image-container">
+//                                     <img class="comment-img" src=${comments[index].img} alt="">
+//                                 </div>
+//                             </div>
+//                             <div class="comment-profile-detail">
+//                                 <p>${comments[index].name}</p>
+//                                 <div class="comment-center">
+            
+//                                 </div>
+//                                 <p>${comments[index].job}</p>
+//                             </div>
+//                         </div>
+            
+//                         <div class="comment-number">
+//                             <p>${comments[index].index}</p>
+//                         </div>
+//                     </div>
+            
+                    
+//                     <div class="comment-details">
+//                         <p>${comments[index].comment}.</p>
+//                     </div>
+//                 </div>
+                
+//                 `
+            
+//             }
+//             if(e.target.parentElement.classList.contains("right-btn")) {
+//                 index++
+//                 if (index === comments.length) {
+//                     index = 0;
+//                 }
+//                 commentSlider.innerHTML = `
+//                 <div class="comment-card">
+//                     <div class="comment-card-header">
+//                         <div class="comment-header-display">
+//                             <div class="comment-profile-image-container">
+//                                 <div class="comment-image-container">
+//                                     <img class="comment-img" src=${comments[index].img} alt="">
+//                                 </div>
+//                             </div>
+//                             <div class="comment-profile-detail">
+//                                 <p>${comments[index].name}</p>
+//                                 <div class="comment-center">
+            
+//                                 </div>
+//                                 <p>${comments[index].job}</p>
+//                             </div>
+//                         </div>
+            
+//                         <div class="comment-number">
+//                             <p>${comments[index].index}</p>
+//                         </div>
+//                     </div>
+            
+                    
+//                     <div class="comment-details">
+//                         <p>${comments[index].comment}.</p>
+//                     </div>
+//                 </div>
+                
+//                 `
+            
+                
+//             }
+//         })
+//     })
+
+
+
+
+
+
+    
+// })
